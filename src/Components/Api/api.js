a69ccc0a3386be4458e043cf69671e9ba504e334
icarogoggin <icaro.goggin@gmail.com>
2021-10-13 23:32:46 -0300

    Criação de endpoints

--- src/Components/Api/api.js
+++ src/Components/Api/api.js
@@ -11,6 +11,30 @@ export const Api = {
 
   updateUrl: (id) => Api.baseUrl + '/games/' + id,
 
+  deleteUrl: id => Api.baseUrl + "/games/" + id,
+  
+  // Endpoint - User
+
+  readByUserIdUrl: (id) => Api.baseUrl + '/users/' + id,
+
+  createUserUrl: () => Api.baseUrl + '/users',
+
+  updateUserUrl: (id) => Api.baseUrl + '/users/' + id,
+
+  deleteUserUrl: id => Api.baseUrl + '/users/' + id,
+
+  // Endpoint - Profile
+
+  readAllProfileUrl: () => Api.baseUrl + '/profiles',
+
+  readByProfileIdUrl: (id) => Api.baseUrl + '/profiles/' + id,
+
+  createProfileUrl: () => Api.baseUrl + '/profiles',
+
+  updateProfileUrl: (id) => Api.baseUrl + '/profiles/' + id,
+
+  deleteProfileUrl: id => Api.baseUrl + '/profiles/' + id,
+
   // GET
   buildApiGetRequest: (url) =>
     fetch(url, {
@@ -35,5 +59,11 @@ export const Api = {
         'Content-type': 'application/json',
       }),
       body: JSON.stringify(body),
+    }),
+
+  // DELETE
+  buildApiDeleteRequest: (url) =>
+    fetch(url, {
+      method: 'DELETE',
     }),
 };
